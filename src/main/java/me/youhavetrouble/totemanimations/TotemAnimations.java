12402b00d75d55a8a44239aff1dcf0abdddf0657
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2022-09-21 18:24:12 +0200

    APified the animation method, made sure lower java versions can run it

--- src/main/java/me/youhavetrouble/totemanimations/TotemAnimations.java
+++ src/main/java/me/youhavetrouble/totemanimations/TotemAnimations.java
@@ -1,5 +1,10 @@
 package me.youhavetrouble.totemanimations;
 
+import org.bukkit.EntityEffect;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TotemAnimations extends JavaPlugin {
@@ -7,11 +12,21 @@ public final class TotemAnimations extends JavaPlugin {
     @Override
     public void onEnable() {
         getCommand("totemanimation").setExecutor(new TotemCommandExecutor());
-
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    /**
+     * Shows totem animation with specified CustomModelData.
+     * @param player Player to show animation to
+     * @param customModelData Totem CustomModelData value
+     */
+    public static void playTotemAnimation(Player player, int customModelData) {
+        ItemStack totem = new ItemStack(Material.TOTEM_OF_UNDYING);
+        ItemMeta meta = totem.getItemMeta();
+        meta.setCustomModelData(customModelData);
+        totem.setItemMeta(meta);
+        ItemStack hand = player.getInventory().getItemInMainHand();
+        player.getInventory().setItemInMainHand(totem);
+        player.playEffect(EntityEffect.TOTEM_RESURRECT);
+        player.getInventory().setItemInMainHand(hand);
     }
 }
--- src/main/java/me/youhavetrouble/totemanimations/TotemCommandExecutor.java
+++ src/main/java/me/youhavetrouble/totemanimations/TotemCommandExecutor.java
@@ -1,14 +1,10 @@
 package me.youhavetrouble.totemanimations;
 
 import org.bukkit.Bukkit;
-import org.bukkit.EntityEffect;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -17,9 +13,6 @@ import java.util.List;
 
 public class TotemCommandExecutor implements TabExecutor {
 
-    private final ItemStack totem = new ItemStack(Material.TOTEM_OF_UNDYING);
-    ItemMeta meta = totem.getItemMeta();
-
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length != 2) return false;
@@ -27,7 +20,7 @@ public class TotemCommandExecutor implements TabExecutor {
         if (player == null) return false;
         try {
             int customModelData = Integer.parseInt(args[1]);
-            playTotemAnimation(player, customModelData);
+            TotemAnimations.playTotemAnimation(player, customModelData);
         } catch (NumberFormatException e) {
             return false;
         }
@@ -37,26 +30,15 @@ public class TotemCommandExecutor implements TabExecutor {
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         List<String> suggestions = new ArrayList<>();
-
         if (args.length == 1) {
             Bukkit.getOnlinePlayers().forEach(player -> {
-                if (sender instanceof Player senderPlayer) {
+                if (sender instanceof Player) {
+                    Player senderPlayer = (Player) sender;
                     if (!senderPlayer.canSee(player)) return;
                     suggestions.add(player.getName());
                 }
             });
         }
-
         return suggestions;
     }
-
-    public void playTotemAnimation(Player player, int customModelData) {
-        meta.setCustomModelData(customModelData);
-        totem.setItemMeta(meta);
-        ItemStack hand = player.getInventory().getItemInMainHand();
-        player.getInventory().setItemInMainHand(totem);
-        player.playEffect(EntityEffect.TOTEM_RESURRECT);
-        player.getInventory().setItemInMainHand(hand);
-    }
-
 }
